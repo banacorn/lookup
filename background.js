3214f021ee69a4cba3630f1b5ebe526ceea801e7
Tîng-Giān Luā <banacorn@gmail.com>
2016-08-01 01:41:47 +0800

    fixed

--- background.js
+++ background.js
@@ -1,51 +1,39 @@
-// // given a tab, determine if it's in German
-// var detectGerman = function(tabId, changeInfo, callback) {
-//     if (changeInfo && changeInfo.status === "complete") {
-//         chrome.tabs.detectLanguage(tabId, (language) => {
-//             if (language === "de") {
-//                 // activate the icon
-//                 chrome.pageAction.show(tabId);
-//                 callback();
-//             }
-//         });
-//     }
-// }
-//
-// // ajax get
-// var get = (word, callback) => {
-//     var xhr = new XMLHttpRequest();
-//     xhr.open("GET", "https://en.wiktionary.org/w/index.php?title=" + word + "&action=raw", true);
-//     xhr.onreadystatechange = function() {
-//         if (xhr.readyState == 4) {
-//             callback(xhr.responseText);
-//         }
-//     }
-//     xhr.send();
-// }
-//
-// // waits for messages coming from the tab
-// var clientMessageListener = (tabId) => (request, sender, sendResponse) => {
-//     if (request.unbind) {
-//         console.log("remove listener!!!");
-//         // chrome.runtime.onMessage.removeListener(clientMessageListener);
-//     } else {
-//         console.log(request)
-//         get(request.word, (response) => {
-//             chrome.tabs.sendMessage(tabId, {
-//                 word: request.word,
-//                 text: response
-//             });
-//         });
-//     }
-// };
-//
-// var tabStateListener = (tabId, changeInfo, tab) => {
-//     // remove old clientMessageListener
-//     chrome.runtime.onMessage.removeListener(clientMessageListener);
-//
-//     detectGerman(tabId, changeInfo, () => {
-//         chrome.runtime.onMessage.addListener(clientMessageListener(tabId));
-//     });
-// }
-//
-// chrome.tabs.onUpdated.addListener(tabStateListener);
+// given a tab, determine if it's in German
+var detectGerman = function(tabId, changeInfo, callback) {
+    if (changeInfo && changeInfo.status === "complete") {
+        chrome.tabs.detectLanguage(tabId, (language) => {
+            if (language === "de") {
+                // activate the icon
+                chrome.pageAction.show(tabId);
+                callback();
+            }
+        });
+    }
+}
+
+// ajax get
+var get = (word, callback) => {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://en.wiktionary.org/w/index.php?title=" + word + "&action=raw", true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            callback(xhr.responseText);
+        }
+    }
+    xhr.send();
+}
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+
+    detectGerman(tabId, changeInfo, () => {
+        console.log("opened");
+        var port = chrome.tabs.connect(tabId, {name: "woerterbuch"});
+
+        port.onMessage.addListener((message) => {
+            console.log(message);
+            get(message, (response) => {
+                port.postMessage(response);
+            });
+        });
+    });
+});
--- content.js
+++ content.js
@@ -1,99 +1,94 @@
-// var sectionize = (text) => {
-//     // the tail you've met in Haskell
-//     var tail = (list) => {
-//         var result = [];
-//         for (var i = 1; i < list.length; i++) {
-//             result.push(list[i]);
-//         }
-//         return result;
-//     }
-//
-//     var collectSections = (text, regexs) => {
-//         if (regexs.length === 0) {
-//             return {
-//                 paragraph: text,
-//                 sections: {}
-//             }
-//         } else {
-//             var obj = {
-//                 paragraph: "",
-//                 sections: {}
-//             };
-//             var splitted = text.split(regexs[0]);
-//             for (var [index, value] of splitted.entries()) {
-//                 if (index === 0) {
-//                     obj.paragraph = splitted[0];
-//                 }
-//                 if (index % 2 === 1) {
-//                     obj.sections[value] = collectSections(splitted[index + 1], tail(regexs));
-//                 }
-//             }
-//             return obj;
-//         }
-//     }
-//
-//     var h2regex = /(?:\s\s\-\-\-\-\s\s)?\=\=([^\=]+)\=\=\s/g;
-//     var h3regex = /\=\=\=([^\=]+)\=\=\=\s/g;
-//     var h4regex = /\=\=\=\=([^\=]+)\=\=\=\=\s/g;
-//
-//     return collectSections(text, [h2regex, h3regex, h4regex]);
-// }
-//
-// var parseWiktionary = function(text) {
-//     var sections = sectionize(text);
-//
-//     var germanSection = sections.sections.German;
-//     // console.log(germanSection)
-//     // return sections
-//     return germanSection
-// }
-//
-// var valid = true;
-// var textSelectionListener = () => {
-//     var word = window.getSelection().toString().trim();
-//     if (word && valid) {
-//         chrome.runtime.sendMessage({word: word});
-//     }
-// };
-//
-// // detect text selections
-// window.addEventListener("beforeunload", () => {
-//     document.removeEventListener("mouseup", textSelectionListener);
-//     valid = false;
-//     chrome.runtime.sendMessage({unbind: true});
-// });
-// document.addEventListener("mouseup", textSelectionListener, false);
-//
-//
-//
-// var printSections = (result) => {
-//     if (typeof result === "string") {
-//         console.log(result)
-//     } else {
-//
-//         // print paragraph
-//         if (result.paragraph) {
-//             console.log(result.paragraph)
-//         }
-//
-//         // print sub-sections
-//         for (header in result.sections) {
-//             console.group(header)
-//             printSections(result.sections[header]);
-//             console.groupEnd()
-//         }
-//     }
-// }
-//
-// chrome.runtime.onMessage.addListener((reply) => {
-//     // clear the old reply
-//     console.clear();
-//
-//     if (reply.text) {
-//         var result = parseWiktionary(reply.text);
-//         console.log("https://en.wiktionary.org/wiki/" + reply.word);
-//         printSections(result)
-//     } else {
-//         console.warn("Not found");
-//     }
-// })
+var sectionize = (text) => {
+    // the tail you've met in Haskell
+    var tail = (list) => {
+        var result = [];
+        for (var i = 1; i < list.length; i++) {
+            result.push(list[i]);
+        }
+        return result;
+    }
+
+    var collectSections = (text, regexs) => {
+        if (regexs.length === 0) {
+            return {
+                paragraph: text,
+                sections: {}
+            }
+        } else {
+            var obj = {
+                paragraph: "",
+                sections: {}
+            };
+            var splitted = text.split(regexs[0]);
+            for (var [index, value] of splitted.entries()) {
+                if (index === 0) {
+                    obj.paragraph = splitted[0];
+                }
+                if (index % 2 === 1) {
+                    obj.sections[value] = collectSections(splitted[index + 1], tail(regexs));
+                }
+            }
+            return obj;
+        }
+    }
+
+    var h2regex = /(?:\s\s\-\-\-\-\s\s)?\=\=([^\=]+)\=\=\s/g;
+    var h3regex = /\=\=\=([^\=]+)\=\=\=\s/g;
+    var h4regex = /\=\=\=\=([^\=]+)\=\=\=\=\s/g;
+
+    return collectSections(text, [h2regex, h3regex, h4regex]);
+}
+
+var parseWiktionary = function(text) {
+    var sections = sectionize(text);
+
+    var germanSection = sections.sections.German;
+    // console.log(germanSection)
+    // return sections
+    return germanSection
+}
+
+
+var printSections = (result) => {
+    if (typeof result === "string") {
+        console.log(result)
+    } else {
+
+        // print paragraph
+        if (result.paragraph) {
+            console.log(result.paragraph)
+        }
+
+        // print sub-sections
+        for (header in result.sections) {
+            console.group(header)
+            printSections(result.sections[header]);
+            console.groupEnd()
+        }
+    }
+}
+
+
+chrome.runtime.onConnect.addListener((port) => {
+
+    // listens to text selection events
+    document.addEventListener("mouseup", () => {
+        var word = window.getSelection().toString().trim();
+        if (word) {
+            // sends request to the background when there's a non-trivial selection
+            port.postMessage(word);
+        }
+    }, false);
+
+    port.onMessage.addListener((reply) => {
+        // clear old results
+        console.clear();
+        if (reply) {
+            var result = parseWiktionary(reply);
+            console.log("https://en.wiktionary.org/wiki/" + reply.word);
+            printSections(result)
+        } else {
+            console.warn("Not found");
+        }
+    });
+});
