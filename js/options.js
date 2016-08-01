3107ac497db00a181418205306675b2ce12e618b
Tîng-Giān Luā <banacorn@gmail.com>
2016-08-01 18:01:44 +0800

    atom-typescript is fucking fat, heavy, slow, and dumb

--- js/options.js
+++ js/options.js
@@ -1,6 +1,40 @@
-var temp = document.querySelector("#temp");
-var displayAllLanguagesCheckbox = document.querySelector("#display-all-languages");
-displayAllLanguagesCheckbox.addEventListener("change", function (event) {
-    temp.textContent = displayAllLanguagesCheckbox.checked;
-    temp.textContent = "fuck";
+// default state
+var settings = {
+    language: "German",
+    displayAllLanguages: false,
+}
+
+// DOM elements
+var languageInput = document.querySelector("#language");
+var displayAllLanguagesInput = document.querySelector("#display-all-languages");
+
+// listeners
+languageInput.addEventListener("change", function (event) {
+    saveSettings();
 });
+displayAllLanguagesInput.addEventListener("change", function (event) {
+    if (displayAllLanguagesInput.checked) {
+        languageInput.setAttribute("disabled", "disabled");
+    } else {
+        languageInput.removeAttribute("disabled");
+    }
+    saveSettings();
+});
+document.addEventListener('DOMContentLoaded', restoreSettings);
+
+// functions
+function saveSettings() {
+    chrome.storage.sync.set({
+        language: languageInput.value,
+        displayAllLanguages: displayAllLanguagesInput.checked
+    });
+}
+
+function restoreSettings() {
+    chrome.storage.sync.get(settings, (items) => {
+        settings = items;
+
+        languageInput.value = settings.language;
+        displayAllLanguagesInput.checked = settings.displayAllLanguages;
+    })
+}
