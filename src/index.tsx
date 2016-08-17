cc7aa9467e1f0c7208f932c48496ee7d63e23a87
Tîng-Giān Luā <banacorn@gmail.com>
2016-08-18 00:44:47 +0800

    install bluebird

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import * as _ from 'lodash';
+import * as Promise from "bluebird";
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider, connect } from 'react-redux';
